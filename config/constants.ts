463ee2f88d22c9aefd0b4105b5e0fafa07b4e152
cozfuttu <canozfuttu07@gmail.com>
2025-01-31 01:31:33 +0300

    chore: config in test directory moved into config directory

---
+++ config/constants.ts
@@ -0,0 +1,3 @@
+export const PRIVATE_KEY_KMS_SIGNER = "388b7680e4e1afa06efbfd45cdd1fe39f3c6af381df6555a19661f283b97de91";
+export const PRIVATE_KEY_COPROCESSOR_ACCOUNT = "7ec8ada6642fc4ccfb7729bc29c17cf8d21b61abd5642d1db992c0b8672ab901";
+export const GATEWAY_URL = "https://gateway.sepolia.zama.ai/";
