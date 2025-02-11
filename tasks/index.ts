c3738b900b2a6a130249e8f88131b9ebd0ad7e79
cozfuttu <canozfuttu07@gmail.com>
2025-02-11 22:05:54 +0300

    feat: allowance script ready

--- tasks/index.ts
+++ tasks/index.ts
@@ -2,4 +2,5 @@ export * from "./ZamaBridge/bridge";
 export * from "./cERC20/balanceOf";
 export * from "./cERC20/wrap";
 export * from "./cERC20/approve";
+export * from "./cERC20/allowance";
 export * from "./cERC20/transferFrom";
